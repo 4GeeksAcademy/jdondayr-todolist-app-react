e02362940392d76e4edb3903b4a6d274df4c57d7
Juan <jdondayr@gmail.com>
2026-06-07 19:26:19 +0200

    All JSX and logic part done

---
+++ src/js/components/Todolist.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Todolist = () => {
+
+    // useStates
+    const [taskInputValue, setTaskInputValue] = useState("");
+    const [itemsLeft, setItemsLeft] = useState(0);
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = (ev) => {
+        if (ev.key === "Enter") {
+            setTasks([...tasks, taskInputValue]);
+            setTaskInputValue("");
+            setItemsLeft(itemsLeft + 1);
+        }
+    }
+
+    const removeTask = (indexToRemove) => {
+        setTasks(tasks.filter((task, index) => index != indexToRemove));
+        setItemsLeft(itemsLeft - 1);
+    }
+
+    const tasksList = tasks.map((task, index) => <li key={index} className="task">{task}<span className="close-button" onClick={()=>removeTask(index)}>x</span></li>)
+
+    return (
+        <div className="todo-list">
+            <ul className="list">
+                <input type="text" onChange={(ev) => setTaskInputValue(ev.target.value)} onKeyDown={addTask} value={taskInputValue} />
+                {tasksList}
+            </ul>
+            <span className="items-left">{itemsLeft >= 1 ? `${itemsLeft} items left` : "No hay tareas pendientes, por favor añade una tarea"}</span>
+        </div>
+    )
+}
+
+export default Todolist;
